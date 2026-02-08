bc24baf0468755dc03cca2202fee4792c73acba7
Cvijander <cvija85@gmail.com>
2026-02-08 13:16:16 +0100

    "Variables"

---
+++ Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x = 7; 
+int y ;
+
+y = x + 3;
+
+Console.WriteLine(y);
+
+
+Console.WriteLine("What is your name? ");
+Console.Write("Type your first name: ");
+string myFirstName = Console.ReadLine();
+
+string myLastName;
+Console.Write("Type your last name: ");
+myLastName = Console.ReadLine();
+
+
+Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
