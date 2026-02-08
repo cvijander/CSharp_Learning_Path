b3d03c5644794ee24f7a0c1ded31017f8404a8c1
Cvijander <cvija85@gmail.com>
2026-02-08 22:24:23 +0100

    "Operators variables "

---
+++ Operators/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// varialbe declaration 
+int x, y, a, b;
+
+// asignment operator 
+x = 3;
+y = 2;
+a = 1;
+b = 0;
+
+// there are many mathematicla operators in C#
+
+// Addition operator 
+x = 3 + 4;
+
+// substraction operator 
+x = 4 - 3;
+
+// multiplication operator
+x = 4 * 3;
+// division operator 
+
+x = 4 / 2;
+
+// order of operation using parethesis 
+x = (x + y) * (a - b);
+
+// there are many operators used to evaluate values .. 
+
+// equality operator 
+if (x ==y )
+{
+       Console.WriteLine("x and y are equal");
+
+}
+
+// greater than operator 
+if (x > y )
+{
+
+}
+
+// less than operator 
+
+if (x < y)
+{
+
+}
+
+// greater or equal to operator 
+if (x >= y)
+{
+
+}
+
+// less than or equal to operator
+if (x <= y)
+{
+}
+
+// There are conditional operators as well that can be used to expand / enhance an evaluation ..
+// adn they can be combined together multiple times. 
+
+// conditional AND operator 
+if ((x > y) && (a > b))
+{
+
+}
+
+// conditional OR operator 
+if((x > y) || (a > b))
+{
+
+}
+
+// aslo , here 's the in-line conditional operator we learedn about in the previous lession 
+string message = (x == 1) ? "Car" : "Boat";
+
+// member access and method invocation 
+Console.WriteLine(message);
+Console.WriteLine("Hi");
+
+
+
+
