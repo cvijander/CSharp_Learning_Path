0a659981972597a7cd18bc06168dfe89be1b4279
Cvijander <cvija85@gmail.com>
2026-02-08 18:54:00 +0100

    "If else, conditional operator"

---
+++ Conditional_Operator/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Bob's big giveaway");
+Console.Write("Choose a door: 1, 2 or 3 : ");
+string userValue = Console.ReadLine();
+
+
+// conditional operator
+string message = (userValue == "1") ? "boat" : "strand of ligth";
+
+
+Console.Write("You won a ");
+Console.Write(message);
+Console.Write(".");
+
+Console.WriteLine("You won a  {0} .", message);
+Console.WriteLine($"You won a {message}." );
+Console.ReadLine();
+
---
+++ Decision/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bob's Big Giveaway");
+Console.Write("Choose a door: 1, 2 or 3: ");
+
+string userValue = Console.ReadLine();
+
+string message = "";
+
+if (userValue == "1")
+{
+    message = "You won a new car";    
+}
+
+else if (userValue == "2")
+{
+     message = "You won a new boat";    
+}
+
+else if (userValue == "3")
+{
+     message = "You won a new cat";    
+}
+
+else
+{
+     message = "Sorry, we didn't understand.";
+    //message = message + " You lose.";
+    message += "You lose ";
+}
+
+Console.WriteLine(message);
+Console.ReadLine();
