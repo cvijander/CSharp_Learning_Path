f8d21e74227f2e63bb92d365da92047a194881a3
Cvijander <cvija85@gmail.com>
2026-02-15 15:23:21 +0100

    "For loop"

---
+++ For_Iteration/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for ( int i = 0; i < 10; i++ )
+{
+    Console.WriteLine(i);
+    if (i == 7)
+    {
+        Console.WriteLine("Found 7");
+        //break;  - prekidamo petlju, iskacemo iz nje 
+        //continue; - nasttavljamo dalje sa svim preostalim brojevima
+    }
+}
+
+Console.ReadLine();
