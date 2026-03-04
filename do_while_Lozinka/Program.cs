ee117fa654176ac081ca2cd0fba899474685ebfe
Cvijander <cvija85@gmail.com>
2026-03-04 08:12:25 +0100

    jos par vezbica

---
+++ do_while_Lozinka/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        KreirajLozinku();
+    }
+
+    private static void KreirajLozinku()
+    {
+        bool neispravnaLozinka = true;
+
+        do
+        {
+            Console.WriteLine("Molimo unesite minimuk 8 karaktera  -  napomena * razmak na pocetku i na kraju se ne racuna u lozinku ");
+            string userInput = Console.ReadLine().Trim();
+
+            if (userInput.Length >= 8)
+            {
+                neispravnaLozinka = false;
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Molimo unesite 8 karaktera ");
+                neispravnaLozinka = true;
+            }
+
+
+        } while (neispravnaLozinka);
+
+        Console.WriteLine("Hvala na koriscenjeu naseg programa");
+
+
+    }
+}
--- do_while_tezine_paketa/Program.cs
+++ do_while_tezine_paketa/Program.cs
@@ -2,6 +2,37 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        KontrolaTezine();
+    }
+
+    private static void KontrolaTezine()
+    {
+        int tezina = 0;
+
+        bool neispravanPaket = true;
+
+        do
+        {
+            Console.WriteLine("Unesite tezinu paketa (dozvoljeno od 1 do 20 kg )");
+            int userTezina = int.Parse(Console.ReadLine().Trim());
+
+            if (userTezina < 1 || userTezina > 20)
+            {
+                neispravanPaket = true;
+                Console.WriteLine($"Vasa tezina je {userTezina} ");
+                tezina += userTezina;
+            }
+            else
+            {
+                neispravanPaket = false;
+                Console.WriteLine("Greska");
+            }
+
+
+        } while (neispravanPaket);
+
+        Console.WriteLine($"Hvala sto ste koristili program ukupno ste poslali {tezina} kg. ");
+
+
     }
 }
