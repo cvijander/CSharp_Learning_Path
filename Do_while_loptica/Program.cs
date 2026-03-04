23845c399d089f2940d8d711f5cb8a709d698077
Cvijander <cvija85@gmail.com>
2026-03-04 07:27:39 +0100

    jos par do while petlji

---
+++ Do_while_loptica/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BacanjeLoptice();
+    }
+
+    private static void BacanjeLoptice()
+    {
+        int brojac = 0;
+
+        bool doneoLopticu = true;
+
+        do
+        {
+            brojac++;
+            Console.WriteLine($"Bacio si lopticu. Ovo je bacanje broj {brojac}.");
+
+            Console.WriteLine("Da li je Vucko doneo lopticu ? DA / NE");
+            string userInput = Console.ReadLine().Trim().ToLower();
+
+            if (userInput == "da")
+            {
+                doneoLopticu = true;
+            }
+            else
+            {
+                doneoLopticu = false;
+            }
+
+        }
+        while (doneoLopticu);
+
+        Console.WriteLine($"Vucko se umorio. Ukupno bacanja {brojac}");
+    }
+    
+}
---
+++ do_while_kalkulator_za_komponente/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        KalkulatorKomponenti();
+    }
+
+    private static void KalkulatorKomponenti()
+    {
+        int total = 0;
+
+        bool josKomponenata = true;
+
+        do
+        {
+            Console.WriteLine("Unesite cenu za sledecu komponentu: ili vec 0 za kraj kupovine ");
+            int cena = int.Parse(Console.ReadLine().Trim());
+
+            if (cena != 0)
+            {
+                total = total +cena;
+                josKomponenata = true;
+            }
+            else
+            {
+                josKomponenata = false;
+            }
+           
+        } while (josKomponenata);
+
+        Console.WriteLine($"Vas racun je {total}");
+    }
+}
