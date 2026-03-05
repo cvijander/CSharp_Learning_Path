3593806ea533e695aaf1805cc4473eb386de52b8
Cvijander <cvija85@gmail.com>
2026-03-05 07:35:41 +0100

    do while jos neki primer

---
+++ do_while_skopi_pc/Program.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SklopiPC();
+    }
+
+    private static void SklopiPC()
+    {
+        bool josKomponenti = true;
+
+
+        do
+        {
+            Console.WriteLine("Unesite neki od brojeva ili vec 99 za kraj ");
+            string userInput = Console.ReadLine().Trim();
+
+            josKomponenti = ObradiOdgovor(userInput);
+
+
+        } while (josKomponenti);
+
+        Console.WriteLine("Hvala na koriscenju");
+
+    }
+
+    private static bool ObradiOdgovor(string input )
+    {
+        switch (input)
+        {
+            case "1":                
+                Console.WriteLine("Dodali ste procesor");
+                return true;
+
+
+            case "2":
+                Console.WriteLine("Maticna");
+                return true;
+
+
+            case "3":
+                
+                Console.WriteLine("Napajanje");
+                return true;
+
+
+            case "99":
+                
+                Console.WriteLine("Kraj");
+                return false;
+
+            default:
+                return true;
+
+        }
+    }
+}
