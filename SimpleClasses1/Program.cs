bdcdb4b9cb4eb1f5adfca70bf7b426ad9c9d4cbf
Cvijander <cvija85@gmail.com>
2026-03-07 11:25:51 +0100

    clase ponovo

---
+++ SimpleClasses1/Program.cs
@@ -0,0 +1,59 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car myCar = new Car();
+
+        myCar.Make = "Oldsmobile";
+        myCar.Model = "Cutlas Supreme";
+        myCar.Year = 1986;
+        myCar.Color = "Silver";
+
+        Console.WriteLine($" {myCar.Make}  {myCar.Model}  {myCar.Year}  {myCar.Color}");
+
+        decimal value = Determine(myCar);
+
+        Console.WriteLine(value);
+
+        Console.WriteLine($" This is method from my class , and my car is valued at  {myCar.DetermineMarketValue():C}");
+
+        Console.ReadLine();
+
+    }
+
+    private static decimal Determine(Car car)
+    {
+        decimal carValue = 100;
+
+        return carValue;
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+
+            if (Year > 1990)
+            {
+                carValue = 10000;
+            }
+            else
+            {
+                carValue = 2000;
+            }
+
+            return carValue;
+        }
+
+
+    }
+}
---
+++ SimpleClasses2/Program.cs
@@ -0,0 +1,58 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Car myCar = new Car();
+        myCar.Model = "Odsmobile";
+        myCar.Make = "Cutlas Supreme";
+        myCar.Year = 1986;
+        myCar.Color = "Silver";
+
+        Console.WriteLine($"{myCar.Model}  {myCar.Make}  {myCar.Color}  {myCar.Year}");
+
+        Console.WriteLine($"{Determine(myCar):C}");
+
+        Console.WriteLine($"Determince car from car method {myCar.DetermineMarketValue():C}");
+
+        Console.ReadLine();
+
+    }
+
+    private static decimal Determine(Car car)
+    {
+        decimal value = 100;
+        return value;
+    }
+
+
+}
+
+class Car
+{
+    public string Make { get; set; }
+
+    public string Model { get; set; }
+
+    public int Year { get; set; }
+
+    public string Color { get; set; }
+
+    public decimal DetermineMarketValue()
+    {
+        decimal carValue;
+
+        if (Year > 2000)
+        {
+            carValue = 10000;
+        }
+        else
+        {
+            carValue = 2000;
+        }
+
+        return carValue;
+    }
+
+
+}
