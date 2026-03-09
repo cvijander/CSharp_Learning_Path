1274949469454f7488b63719d06b6cbed43aa52f
Cvijander <cvija85@gmail.com>
2026-03-09 06:56:33 +0100

    car class

---
+++ SimpleClasses3/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car myCar = new Car();
+
+        myCar.Make = "Oldsmobile";
+        myCar.Model = "Cutlas supreme";
+        myCar.Year = 1986;
+        myCar.Color = "Silver";
+
+        Console.WriteLine($"my fancy car is {myCar.Make}  {myCar.Model}   {myCar.Year}  {myCar.Color}");
+
+        // Console.WriteLine($"{DetermineMarketValue(myCar):C}");
+
+        Console.WriteLine($"{myCar.DetermineMarket():C}");
+        Console.ReadLine();
+    }
+
+    private static decimal DetermineMarketValue(Car car)
+    {
+        decimal value = 100;
+        return value;
+    }
+
+}
+
+class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+
+
+    public decimal DetermineMarket()
+    {
+        decimal carValue;
+
+        if (Year > 1990)
+        {
+            carValue = 10000;    
+        }
+        else
+        {
+            carValue = 200;
+        }
+
+        return carValue;
+    }
+
+
+}
