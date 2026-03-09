59d6f5d80346e84b2937dd30dbb21647e6103ea3
Cvijander <cvija85@gmail.com>
2026-03-09 07:54:01 +0100

    constructor i static keywork

--- ObjectLifetime/Program.cs
+++ ObjectLifetime/Program.cs
@@ -4,39 +4,50 @@
     {
         Car myCar = new Car();
 
+        Car myThirdCar = new Car("Honda", "Civic", 2005, "Yellow");
 
-        myCar.Make = "Oldsmobile";
-        myCar.Model = "Cutlas Supreme";
-        myCar.Year = 1986;
-        myCar.Color = "Silver";
+        /*
+                myCar.Make = "Oldsmobile";
+                myCar.Model = "Cutlas Supreme";
+                myCar.Year = 1986;
+                myCar.Color = "Silver";
 
-        Car myOtherCar;
+                Car myOtherCar;
 
-        myOtherCar = myCar;
+                myOtherCar = myCar;
 
 
-        Console.WriteLine($"{myCar.Make} {myCar.Model}  {myCar.Year}  {myCar.Color}");
+                Console.WriteLine($"{myCar.Make} {myCar.Model}  {myCar.Year}  {myCar.Color}");
 
-        Console.WriteLine($"{myOtherCar.Make} {myOtherCar.Model}  {myOtherCar.Year} {myOtherCar.Color}");
+                Console.WriteLine($"{myOtherCar.Make} {myOtherCar.Model}  {myOtherCar.Year} {myOtherCar.Color}");
 
-        
 
-        myOtherCar.Model = "98";
 
-        Console.WriteLine("------------------");
-        Console.WriteLine($"{myCar.Make} {myCar.Model}  {myCar.Year}  {myCar.Color}");
+                myOtherCar.Model = "98";
 
-        Console.WriteLine($"{myOtherCar.Make} {myOtherCar.Model}  {myOtherCar.Year} {myOtherCar.Color}");
+                Console.WriteLine("------------------");
+                Console.WriteLine($"{myCar.Make} {myCar.Model}  {myCar.Year}  {myCar.Color}");
 
-        Console.WriteLine("------------------");
+                Console.WriteLine($"{myOtherCar.Make} {myOtherCar.Model}  {myOtherCar.Year} {myOtherCar.Color}");
 
-        myOtherCar = null;
+                Console.WriteLine("------------------");
 
-        Console.WriteLine("------------------");
-        Console.WriteLine($"{myCar.Make} {myCar.Model}  {myCar.Year}  {myCar.Color}");
+                myOtherCar = null;
 
-        Console.WriteLine($"{myOtherCar.Make} {myOtherCar.Model}  {myOtherCar.Year} {myOtherCar.Color}");
+                Console.WriteLine("------------------");
+                Console.WriteLine($"{myCar.Make} {myCar.Model}  {myCar.Year}  {myCar.Color}");
 
+                Console.WriteLine($"{myOtherCar.Make} {myOtherCar.Model}  {myOtherCar.Year} {myOtherCar.Color}");
+
+
+
+                Console.ReadLine();
+
+                */
+
+        Console.WriteLine($"{myCar.Make}  {myCar.Model} {myCar.Year}  {myCar.Color}");
+
+        Console.WriteLine($"{myThirdCar.Make}  {myThirdCar.Model} {myThirdCar.Color} {myThirdCar.Year}");
         Console.ReadLine();
 
 
@@ -55,4 +66,23 @@ class Car
     public string Color { get; set; }
 
 
+    // konstruktor
+
+    public Car()
+    {
+        this.Make = "Nissan";
+    }
+
+    public Car(string make,string model, int year , string color)
+    {
+        Make = make;
+        Model = model;
+        Color = color;
+        Year = year;
+    }
+
 }
+
+// https://www.youtube.com/watch?v=0QUgvfuKvWU
+// 440 konstruktor 
+//455 static
