4f61e624e51b570b4e623f2231df5e6c518925f1
Cvijander <cvija85@gmail.com>
2026-03-11 06:50:40 +0100

    lifetime of  an object and references

--- ObjectLifetime1/Program.cs
+++ ObjectLifetime1/Program.cs
@@ -26,11 +26,19 @@
 
         Console.WriteLine("set to null");
 
-        myOtherCar = null;
+       // myOtherCar = null;
 
         Console.WriteLine($" {myCar.Make} {myCar.Model} {myCar.Color} {myCar.Year}");
 
         Console.WriteLine($"{myOtherCar.Make} - {myOtherCar.Model} - {myOtherCar.Color} - {myOtherCar.Year}");
+
+        Car myThirdCar = new Car();
+
+        Car myFourthCar = new Car("Ford", "Mondeo", 1999, "Yellow");
+
+        Console.WriteLine($"{myFourthCar.Make} {myFourthCar.Model} {myFourthCar.Color} {myFourthCar.Year}");
+
+        Console.WriteLine($"{myThirdCar.Make} ");
 
         Console.ReadLine();
 
@@ -49,5 +57,23 @@ class Car
 
     public string Color { get; set; }
 
+    public Car ()
+    {
+        Make = "Nissan";
+        //this.Make = "Nissan"; 
+    }
+
+    public Car (string make, string model, int year, string color )
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        Color = color;
+    }
+
 
 }
+
+//4 46 konstruktyori 
+//430 - lifetime 
+//
---
+++ ObjectLifetime2/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car myCar = new Car();
+
+        myCar.Make = "Oldsmobile";
+        myCar.Model = "Cutlas Supreme";
+        myCar.Year = 1986;
+        myCar.Color = "Silver";
+
+
+        Car myOtherCar;
+        myOtherCar = myCar;
+
+        Console.WriteLine($"{myCar.Make}  {myCar.Model}  {myCar.Color} {myCar.Year}");
+
+        Console.WriteLine($"{myOtherCar.Make} -  {myOtherCar.Model} - {myOtherCar.Color} - {myOtherCar.Year}");
+
+        myOtherCar.Model = "98";
+
+
+        Console.WriteLine($"{myCar.Make}  {myCar.Model}  {myCar.Color} {myCar.Year}");
+
+        Console.WriteLine($"{myOtherCar.Make} -  {myOtherCar.Model} - {myOtherCar.Color} - {myOtherCar.Year}");
+
+        Console.ReadLine();
+
+    }
+
+}
+
+class Car
+{
+    public string Make { get; set; }
+
+    public string Model { get; set; }
+
+    public int Year { get; set; }
+
+    public string Color { get; set; }
+}
