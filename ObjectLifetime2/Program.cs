cc0290d38ab5f0bd7127bb8a27f1a1ad29b07984
Cvijander <cvija85@gmail.com>
2026-03-11 07:28:53 +0100

    Constructor

--- ObjectLifetime2/Program.cs
+++ ObjectLifetime2/Program.cs
@@ -13,12 +13,29 @@
         Car myOtherCar;
         myOtherCar = myCar;
 
+        Car myThirdCar = new Car();
+
+        Car myForthCar = new Car("Toyota", "Supra", 2010, "Purple");
+
+
         Console.WriteLine($"{myCar.Make}  {myCar.Model}  {myCar.Color} {myCar.Year}");
 
         Console.WriteLine($"{myOtherCar.Make} -  {myOtherCar.Model} - {myOtherCar.Color} - {myOtherCar.Year}");
 
+        Console.WriteLine($"{myThirdCar.Make}");
+
+        Console.WriteLine($"{myForthCar.Model}  {myForthCar.Make}  {myForthCar.Color} {myForthCar.Year}");
+
         myOtherCar.Model = "98";
 
+
+        Console.WriteLine($"{myCar.Make}  {myCar.Model}  {myCar.Color} {myCar.Year}");
+
+        Console.WriteLine($"{myOtherCar.Make} -  {myOtherCar.Model} - {myOtherCar.Color} - {myOtherCar.Year}");
+
+        //myOtherCar = null;
+
+        Console.WriteLine("Removed myOtherCar \"handle\"");
 
         Console.WriteLine($"{myCar.Make}  {myCar.Model}  {myCar.Color} {myCar.Year}");
 
@@ -39,4 +56,18 @@ class Car
     public int Year { get; set; }
 
     public string Color { get; set; }
+
+
+    public Car()
+    {
+        Make = "Nissan";
+    }
+
+    public Car(string make, string model, int year, string color )
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        Color = color;
+    }
 }
