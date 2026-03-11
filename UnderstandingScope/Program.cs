33d6262e523799df7e4997ed8f0a2bcd33c5e601
Cvijander <cvija85@gmail.com>
2026-03-11 07:50:36 +0100

    Scope

---
+++ UnderstandingScope/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+
+    private static string k = "";
+
+    private static void Main(string[] args)
+    {
+
+        string j = "";
+        
+        for (int i =0; i < 10; i++)
+        {
+            j = i.ToString();
+            k = i.ToString();
+            Console.WriteLine(i);
+
+
+            if (i == 9)
+            {
+                string l = i.ToString();
+                Console.WriteLine(l);
+            }
+           // Console.WriteLine(l);
+        }
+
+        //Console.WriteLine(i);
+        Console.WriteLine($"Outside of the scope {j} ");
+        Console.WriteLine($"Outside od for loop {k}");
+        HelperMethod();
+        Console.ReadLine();
+        
+    }
+
+    static void HelperMethod()
+    {
+        Console.WriteLine($"Value of k from the Helpermethod():  {k}" );
+    }
+}
+
+
+// 5 sati 
