acdcc021edf1e21f9e9676857edc27e14c301e6b
Cvijander <cvija85@gmail.com>
2026-03-13 07:52:09 +0100

    web scraping with webClient , all the content from front page is now in txt file

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -1,9 +1,17 @@
-﻿internal class Program
+﻿using System.Net;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+
+        WebClient client = new WebClient();
+        string reply = client.DownloadString("https://www.gembird.rs/");
+
+        Console.WriteLine(reply);
+
         string text = "A class is something";
-        System.IO.File.WriteAllText(@"D:\Dev\csharp\CSharp_Learning_Path\AssembliesAndNamespaces\WriteText.txt", text);
+        System.IO.File.WriteAllText(@"D:\Dev\csharp\CSharp_Learning_Path\AssembliesAndNamespaces\WriteText.txt", reply);
 
         Console.WriteLine("Hello, World!");
         Console.ReadLine();
