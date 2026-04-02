f0e7592624caf9d902d9fdad976cbbb27483cb09
Cvijander <cvija85@gmail.com>
2026-04-02 07:42:05 +0200

    try catch exceptions

--- HandlingExceptions1/Program.cs
+++ HandlingExceptions1/Program.cs
@@ -2,8 +2,39 @@
 {
     private static void Main(string[] args)
     {
-        string content  = File.ReadAllText(@"D:\Dev\csharp\CSharp_Learning_Path\HandlingExceptions1\Example.txt");
-        Console.WriteLine(content);
+        try
+        {
+            string content = File.ReadAllText(@"D:\Dev\csharp\CSharp_Learning_Path\HandlingExceptions1\Example.txt");
+            Console.WriteLine(content);
+            
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine($"There was a problem  ");
+            Console.WriteLine("Make sure that the name of the file is named correctly Example.txt");
+            Console.WriteLine(ex.Message);
+        }
+
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine($"There was a problem ");
+            Console.WriteLine("Make sure that directory D:\\Dev\\csharp\\CSharp_Learning_Path\\HandlingExceptions1 exists");
+            Console.WriteLine(ex.Message);
+        }
+
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"There was a problem ");
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            // code to finalize 
+            // setting object to null 
+            // c;psomg database connectisons
+            Console.WriteLine("Closing aplication now ...");
+        }
         Console.ReadLine();
 
     }
