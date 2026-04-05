ada4d2971594826d27d404db43a3e3584d53398f
Cvijander <cvija85@gmail.com>
2026-04-05 11:29:41 +0200

    Event handler in console

--- TimerExample/Program.cs
+++ TimerExample/Program.cs
@@ -8,14 +8,28 @@ internal class Program
         Timer myTimer = new Timer(2000);
 
         myTimer.Elapsed += MyTimer_Elapsed;
+        myTimer.Elapsed += MyTimer_Elapsed1;
 
         myTimer.Start();
+
+        Console.WriteLine("Press Enter to remove red event");
+        Console.ReadLine();
+
+        myTimer.Elapsed -= MyTimer_Elapsed1;
 
         Console.ReadLine();
     }
 
+    private static void MyTimer_Elapsed1(object? sender, ElapsedEventArgs e)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Elapsed {e.SignalTime:HH:mm:ss.fff}");
+    
+     }
+
     private static void MyTimer_Elapsed(object? sender, ElapsedEventArgs e)
     {
+        Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine($"Elapsed {e.SignalTime:HH:mm:ss.fff}");
     }
 }
