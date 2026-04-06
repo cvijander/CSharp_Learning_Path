ddf8b88f1b0a258c975caea50a34394f41e5273d
Cvijander <cvija85@gmail.com>
2026-04-06 07:41:09 +0200

    Lession one - to thingking like an object oriented programmer

--- Lession 1 intro/Program.cs
+++ Lession 1 intro/Program.cs
@@ -3,5 +3,63 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("1. Classes, Objects, Properties, Methods");
+        Console.WriteLine("2. Private, Public, Static, Instance ");
+        Console.WriteLine("3. Inheritance, Abstract, Intefface");
+        Console.WriteLine("4. Object BASED Programming");
+
+        Console.WriteLine();
+
+        Console.WriteLine("OO is a mindset");
+        Console.WriteLine("A philosophy which drives a way of thinking");
+        Console.WriteLine("A world view about problems and solutions");
+        Console.WriteLine("A path with a North star");
+        Console.WriteLine("Guidance and principles on how to achieve overarching objectives");
+
+        Console.WriteLine();
+        Console.WriteLine("Object-oriented programming provides a process on how to discover and implement a conceptual machine to realize that desired output.");
+        Console.WriteLine("How to decompose a problem in the real world and then build some sort of abstraction in this etherial world in our minds ");
+        Console.WriteLine("Abstraction of that problem, and we will build it using principles that will actually sum up important tenants of object-oriented programming. ");
+        Console.WriteLine("Then the end result is that we heva designed conceptually designed machine that takes raw materials like data input from the user, and converts those into thinks like data that has been processed,mesaging, activiteis and so on");
+        Console.WriteLine("Those will have meaning in the living world, even if that is a conceptual world like the account world");
+
+        Console.WriteLine();
+        Console.WriteLine("Software dev religion");
+        Console.WriteLine("In other worlds , ita age old wisdom - not a rulebook - that gives you guidance that you must apply situactionally for your specific problem domain.");
+
+        Console.WriteLine();
+        Console.WriteLine("REquire devotion");
+        Console.WriteLine("People devote their entire careers practicing and learning more about Object Orientation and how to apply it in a give situation");
+
+        Console.WriteLine();
+        Console.WriteLine("Goals");
+        Console.WriteLine("1. Improve Modularity");
+        Console.WriteLine("2. Improve process of analysis, a Design and Communication of a Problem Domain and a Software Solution");
+        Console.WriteLine();
+        Console.WriteLine("Goal is to incease code modularity, so that a change in one module shouldn't breakthings in other modules");
+        Console.WriteLine("Improve code maintance, adding or removing features will not break the entire application. It allows us to withstand the impact of change");
+        Console.WriteLine("It allows us to build new features, and to make changes to existing features faster and more reliably");
+        Console.WriteLine("Enable code reuse");
+        Console.WriteLine("Code Maintenance - adding or removing features, \"change\", faster, more reliably");
+        Console.WriteLine("Enable Code Re-user - packageable and re-useable");
+        Console.WriteLine("Enable Team Collaboration - code broken up into small bits that many can work on, specialists");
+        Console.WriteLine("Enable easier integration - easily abstract and isolate large systems so they can be intechanged");
+        Console.WriteLine();
+        Console.WriteLine("Improve process od analysis, design and communication of a problem domain and software solution");
+        Console.WriteLine("Improving the process of breaking down a problem domain and designing a candidate solution and communiaction it to others.");
+        Console.WriteLine("Using this techiques, these ideas, we begin to look for boundaries in the system and look for the needs and responsabilities");
+        Console.WriteLine("Look for a major players are in the system. It also provides a method of decomposition of a problem into smaller pieces.");
+        Console.WriteLine("Now once we have identified the major players, and we have kind of begin to give them responsabilities .");
+        Console.WriteLine("Here is a need in a system, who should be responsible for it  - further decomposotion to methods.. ");
+
+
+
+
+
+
+
+
+        Console.ReadLine();
     }
 }
