8dd926fdb6f2a95563622e2c58a516eba682cac2
Cvijander <cvija85@gmail.com>
2026-04-10 13:22:29 +0200

    Lession 3 info about the project

--- Lession_3/Program.cs
+++ Lession_3/Program.cs
@@ -8,8 +8,34 @@
             Console.WriteLine();
             Console.WriteLine("The principles and philosophies of object-oriented programming transcend the process of application development");
             Console.WriteLine("When I talk to about the process of application development, I 'm talking about thw primary approaches to the way that project is initiated and then managed");
-            Console.Clear();
-            Console.WriteLine("=== LESSON 2: The Business Side of Software ===");
+            Console.WriteLine();
+            Console.WriteLine("=== LESSON 3: The Business Side of Software ===");
+            Console.WriteLine("The project starts way before the developers are even consulted or there is any initiacion with the developers.");
+            Console.WriteLine();
+            Console.WriteLine(" * In most organizations some need is identified");
+            Console.WriteLine("Perhaps there is deficiency in a current business, or there is some new opportynity that's cropped up that the bussiness needs to take advantage of");
+            Console.WriteLine();
+            Console.WriteLine("As management tries to better understand the problem or the opportunity. They may get people involved who are called bussiness analysts");
+            Console.WriteLine("Bussiness analyst are usually semi-technical people that are more focused on the bussiness side of the technology ");
+            Console.WriteLine("They are interested in systems and  data flow and they are interested from a bussiness perspective");
+            Console.WriteLine("They are usually asked upfront to compile the details in that of the opportunity or of the need into a set of requirements. ");
+            Console.WriteLine("They can make a case to upper management on whether they should move forward and get budget allocated ");
+            Console.WriteLine("These people are often referred to as the stakeholders who initiate this process because they have a vested interest in fixing the problem or pursuing the new opportunity");
+            Console.WriteLine("Those that have most to gain and are going to be on the hook and the most fiscally responsible for the project are usually refered to as the project sponsors");
+            Console.WriteLine();
+            Console.WriteLine("As a part in this early process, the business analyst might even compile all the requirements into a documentation that's sent around to various vendors in the industry");
+            Console.WriteLine("That documents is called  a request for information  - Here what we are looking at. Do you have something that can solve our problem? ");
+            Console.WriteLine("They may even create a follw up document called a request for proposal, this will give more details and request and actual proposal and actual price ");
+            Console.WriteLine("This process of gathering and talking and gathering can take months");
+            Console.WriteLine("If the analyst can find a suitable software product or a system thatn will satisfy their requirment . So they may even decide that it is time to build that to buy a custom solution");
+            Console.WriteLine();
+            Console.WriteLine("Management has to decide to if tis worth time and the money to take this big project on");
+            Console.WriteLine("They may need again to get a budget approved in the next fiscal year. That might require even more time spent upfront in analysis model.");
+            Console.WriteLine("All these need to be determined long before the developers are brought to start ");
+
+
+
+
             Console.WriteLine("Before a programmer writes the first line of code, the business must make decisions.");
             Console.WriteLine("Software does not emerge in a vacuum; it solves a concrete business problem.");
             Console.WriteLine();
@@ -53,3 +79,5 @@
         }
     }
 }
+
+// https://www.youtube.com/watch?v=Tn2mBSo0kRs&list=PLqT8wAi-pOx7X3i6Vynm57MCwFpQWJdP9&index=3
