9b589f700e1a066dd0ee26d61c20f7e207fca40c
Cvijander <cvija85@gmail.com>
2026-04-13 16:15:40 +0200

    Agile

---
+++ AgileMethodology/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace AgileMethodology
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("In late 1990 early 2000s som;ething called extreme programming XP began to pop up.");
+            Console.WriteLine("It emerged as the anthithesisi to big design upfront");
+            Console.WriteLine("Some of the most succesfull parts of extreme programming were extracted out");
+            Console.WriteLine("Some of the ideas weere changed, and some of the proponents of extreme programming adopted a different name calling it now");
+            Console.WriteLine("The Agile methodology");
+            Console.WriteLine();
+            Console.WriteLine("Some companies of today adopt some form of agile, whethere it is very strict form of agile or a looser form of agile.");
+            Console.WriteLine("When you contract Agile to BDEF several things start to emerge as obvious departures from the old guard");
+            Console.WriteLine("1. The development team was involved in the process of analysis and design much earlier in the whole process");
+            Console.WriteLine("2. The domain experts , the people that the bussiness analysts would talk to to determine what the needs of the aplication weere, they would typically be from the actual department, or the department that would be impacted the most from the development of the software ");
+            Console.WriteLine("They will be the one using the software, the end users of it, and they would join the software developers in the same team ");
+            Console.WriteLine("3. When developers had questions they would get the answer immediately, or the y could raise the question tho the people that they knwe back in their old department that would know the answer to that particular question");
+            Console.WriteLine("4. The development team would outline a series of iterations or the projects manageer woudl do series of iterations one week , two weeks iterations");
+            Console.WriteLine("Really short period of time,relative to most iterations up to that point, they call them \"sprints\"");
+            Console.WriteLine("THe bussiness analyst, the developers, the project manager, they all sit around , they would collect these requirements as they talk throught what the system shoudl do ");
+            Console.WriteLine("They collect those requirements on a series of index cards and tht to keep them all scoped");
+            Console.WriteLine("They will use those index cards with programmers to say how long do you think would need us to implement that particular feature .");
+            Console.WriteLine("It will give the project manager and the analyst and maybe even the subject matter expert some idea of what the feature and they would come up with some time estimate");
+            Console.WriteLine("Those estimates were taken and presented to the sterring commiette.");
+            Console.WriteLine("Thet would say, first iteration we have this many hours and these are the tasks and how many hours they should take , which feature do you need implemented first");
+            Console.WriteLine("This gave the management a better insight into where they were actually in the development process, how far ahead or behind they were. ");
+            Console.WriteLine("That manages some informations about what challenges lie ahead, and they would talk about what hurdles need to be removed. ");
+            Console.WriteLine("The project manager would use this as an opprtunity to kind of keep thie finger on the pulse of where the project was really at and where the challenges were and if the estimates were in fact correct");
+            Console.WriteLine("The project manager now has a lot of data, he got lots of feedback from his team, he is able to keep a board that shows every task,who is responsible for it, how far it is along those are called ");
+            Console.WriteLine("So in any point in time , in a very visible way throught large boards or software , they would know and be able to communicate exactly where the project status was at");
+            Console.WriteLine("At the end of a day you had to come out witha working software.");
+            Console.WriteLine("At the end of the iteration or the sprint, the software had to work and it had to be demonstable. It had to do something");
+            Console.WriteLine("Hopefully all of the tasks that weere committed to were deployed and demonstable to the steering committee.");
+            Console.WriteLine("Any changes can be address in much more sooner in the development process. And then once you finished that sprint you got the feedback, the process would start all over again");
+            Console.WriteLine();
+            Console.WriteLine("With the agile software development process , a lot of emphasisi is made on embracing change , constants refactoring ");
+            Console.WriteLine("Unit testing really makes this possible to constantly check and make sure that any changes that you mande to the software does not break anything else.");
+            Console.WriteLine("If it break something it happened in last lines of code, it can identify the issuess much more early in the process than waitinig to the very end of the project to do your testing.");
+
+
+
+
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
+
+
+// https://www.youtube.com/watch?v=S1ZKv3c1DSI&list=PLqT8wAi-pOx7X3i6Vynm57MCwFpQWJdP9&index=5
