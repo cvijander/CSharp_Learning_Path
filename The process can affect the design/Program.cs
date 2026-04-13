f9e5304d2806285fea327cae902f9ed69d9c5ee0
Cvijander <cvija85@gmail.com>
2026-04-13 21:53:49 +0200

    Lession 6 half

---
+++ The process can affect the design/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace The_process_can_affect_the_design
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How an object-oriented design is created because it is going to affect how we understand design");
+            Console.WriteLine("The design is essentially complete before anyone begins to write code ");
+            Console.WriteLine("You end up with massive design documentation and those documents because of how much there is. It is just difficult to comprehend");
+            Console.WriteLine("Wrap your brain around and it potentially becomes outdated and obsolete before the end of the project.");
+            Console.WriteLine("Design that look great on papar can be deficult to implement or have unintended consequnces");
+            Console.WriteLine("BDUF design tends to be more brittle. It does not account for the invetabillity of change");
+            Console.WriteLine("When change comes it potentially destroys weeks and months worth of work");
+            Console.WriteLine(" but in more agile approach, the design is revealed in smaller, more digestible units of time and work in concerte with actual implemetnation of software");
+            Console.WriteLine("And the benefit of that is it is happening while there 's still time to adjust the direction and we don't have to throw out ");
+            Console.WriteLine("So the team worries about this week's iteration and keeps the design flexible knowing that change is going to come");
+            Console.WriteLine("It builds change into the software development process treating it as an inevitabillity. It embraces the inevitability of change");
+            Console.WriteLine();
+
+
+
+
+
+        }
+    }
+}
