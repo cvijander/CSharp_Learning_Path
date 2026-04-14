3e8f9ae1fd982f14eb340514578a5cdcbb501fe8
Cvijander <cvija85@gmail.com>
2026-04-14 07:37:16 +0200

    Lession 6 full

--- The process can affect the design/Program.cs
+++ The process can affect the design/Program.cs
@@ -12,12 +12,31 @@
             Console.WriteLine("Design that look great on papar can be deficult to implement or have unintended consequnces");
             Console.WriteLine("BDUF design tends to be more brittle. It does not account for the invetabillity of change");
             Console.WriteLine("When change comes it potentially destroys weeks and months worth of work");
+            Console.WriteLine();
+            Console.WriteLine("Agile approach");
             Console.WriteLine(" but in more agile approach, the design is revealed in smaller, more digestible units of time and work in concerte with actual implemetnation of software");
             Console.WriteLine("And the benefit of that is it is happening while there 's still time to adjust the direction and we don't have to throw out ");
             Console.WriteLine("So the team worries about this week's iteration and keeps the design flexible knowing that change is going to come");
             Console.WriteLine("It builds change into the software development process treating it as an inevitabillity. It embraces the inevitability of change");
             Console.WriteLine();
+            Console.WriteLine("In a more BDUF approach, design is done more way in advance on paper.It is codified in some alternative language, some alternative tool.  ");
+            Console.WriteLine("Those alternative expressions of the design have to be maintained constantly in order for that design to remain relevent to the system.");
+            Console.WriteLine("Much time and effort is spent in either keeping the design documents relative relevant or the more likely scenario is that the development team begins to distrust the design documentation because they no longer reflects the reality of the business needs");
+            Console.WriteLine();
+            Console.WriteLine("In an agile approach the is the design. It is the documentation of how business works today.");
+            Console.WriteLine("It evolve quickly and the code that express the business rules of the business much must keep pace with it. ");
+            Console.WriteLine();
+            Console.WriteLine("In a more BDUF approach there is a distinct period of analysis, a period of design, and then period of development, and then period of testing ");
+            Console.WriteLine("But in agile approach those lines are blured, all kind happens in the same time, in a more organic fashion. The design is discovered over time, andi it is less structured, more fluid process that blend analysis, design, implementation and testing");
+            Console.WriteLine();
+            Console.WriteLine("Who is doing the designing and when they are creating the design does have some impact on the ultimate design of the system. THe more design that happens without the involment of the developers means more brittle design and it becomes a huge risk in the project");
+            Console.WriteLine();
+            Console.WriteLine("Object oriented programming is about creating flexible interchangeable objects that decrease the complexity of implementation and decrease the resistance of the application to the effects of change");
+            Console.WriteLine("it eases the maintenance of the system, makes it more resilient to the impact and the inevitability of change long term");
 
+
+
+            Console.ReadLine();
 
 
 
@@ -25,3 +44,5 @@
         }
     }
 }
+
+// https://www.youtube.com/watch?v=hj-77d45HH8&list=PLqT8wAi-pOx7X3i6Vynm57MCwFpQWJdP9&index=6
