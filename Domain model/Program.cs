e962daec183678e6068ec74681ef64cb2606ae9b
Cvijander <cvija85@gmail.com>
2026-04-14 08:06:40 +0200

    Lession 7 Abstracting the Real World into a Domain Model

--- Domain model/Program.cs
+++ Domain model/Program.cs
@@ -5,6 +5,27 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Our measure of success lies in how clearly we invent a software reality that satisfies our application requirements - and not how closely it resembles the real world");
+            Console.WriteLine();
+            Console.WriteLine("Object  oriented programming models the real world and that is what makes it so powerful and so easy to learn and how you simplify the complex problems because you have a real world things that you want to model");
+            Console.WriteLine("Then it is easy to model in etherleal conceptual world. ");
+            Console.WriteLine("You want to model an animal, it has 4 legs, feet, eyes, mouth, nose, method - talk  , and it can derive dog from animal class, also an a  cat , etc ");
+            Console.WriteLine("But you can override and specialize them with example speak, the dog and the cat speaks different ");
+            Console.WriteLine();
+            Console.WriteLine("But it is a lie - it is a kind of misnomer");
+            Console.WriteLine("You dont model the real world necessarily. You create a model that satisfies your applications requirements");
+            Console.WriteLine("We take an abstraction of the real world and we call out those details that are pertinent to our applications needs");
+            Console.WriteLine("Example of a  car, we can model it in number different ways");
+            Console.WriteLine("Are we modeling a caf from a factory persepctive, maybe from a car lot,or from aspect of a service station or repair facility. Maybe even and inventory parts ");
+            Console.WriteLine("We are modeling a car from the context of a traffic simulator, maybe we are really modeling specific roads and bussiness and residental areas and speed limit , maybe we are modeling a car from a context of a self-driving car software");
+            Console.WriteLine("4:00  ukupno 7 minuta ");
+
+
+
+
         }
     }
 }
+
+
+// https://www.youtube.com/watch?v=flIDpf-89n4&list=PLqT8wAi-pOx7X3i6Vynm57MCwFpQWJdP9&index=7
