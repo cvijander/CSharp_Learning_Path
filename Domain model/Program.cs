f6f24d550bba38e060ec3669afa8c66cd192ab68
Cvijander <cvija85@gmail.com>
2026-04-15 08:11:30 +0200

    Roles of an objects

--- Domain model/Program.cs
+++ Domain model/Program.cs
@@ -18,7 +18,17 @@
             Console.WriteLine("Example of a  car, we can model it in number different ways");
             Console.WriteLine("Are we modeling a caf from a factory persepctive, maybe from a car lot,or from aspect of a service station or repair facility. Maybe even and inventory parts ");
             Console.WriteLine("We are modeling a car from the context of a traffic simulator, maybe we are really modeling specific roads and bussiness and residental areas and speed limit , maybe we are modeling a car from a context of a self-driving car software");
-            Console.WriteLine("4:00  ukupno 7 minuta ");
+            Console.WriteLine("You can see that even though there is a real world car, there are many different facets and abstractions of it. And the way that we model the car in our application will depend on our the need of our application");
+            Console.WriteLine("We  abstract those facets out into a model that describes the primary areas of focus inside of our problem domain");
+            Console.WriteLine("We are inventing the object machinery as the book would put it. That object machinery fills a much richer set of concepts that what a paper forme would at first glance require");
+            Console.WriteLine("Object oriented programming is not relly about modeling the real world per se. ");
+            Console.WriteLine("It's althought there  that may have beeon a convinient way to understand it from a highlevel perspective as you were getting started. It helped you undestand classes , methods, properties");
+            Console.WriteLine("Object oriented programming is about abstracting the real world into a subset perhaps a superset of objects that we need to consider for a specific problem domain and haow to implement it as a software system.");
+            Console.WriteLine("So in some cases those objects will be our own creatinon deerived from our imagination that don't have physical or real world equivalents");
+            Console.WriteLine("We create these abstractions because we are building conceptual machines. Machines that address ethreal conceptual needs like process, state, display, validation, persistence ");
+
+
+            Console.ReadLine();
 
 
 
---
+++ Roles/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Roles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("APPLICATION DESIGN");
+            Console.WriteLine("Using several different complimentary perspectives");
+            Console.WriteLine();
+            Console.WriteLine("Application    - A set of interacting objects");
+            Console.WriteLine("Object         - An implementation of one or more roles");
+            Console.WriteLine("Role           - A set of related responsibilities");
+            Console.WriteLine("Responsability - An obligation to perform a task or know information");
+            Console.WriteLine("Collaboration  - An inraction of objects or roles (or both)");
+            Console.WriteLine("Contract       - An agreement outlining the terms of collaboration");
+
+
+            Console.WriteLine("Each object plays a role. It plays a part of larger conceptual machine");
+            Console.WriteLine("We delegate one or more responsabilities to an object and it now has the obligation to  take care or that task or to hold that information");
+            Console.WriteLine("We are depending on it to do its job, so that the machine as a whole can function correctly. If he does not do its job, then the machine will have an issue");
+            Console.WriteLine("We have a need and we delegate the responsibilities to take care of that need to an object ");
+            Console.WriteLine("Whenever we devise our conceptual machine, there might be several different objects. Perhaps i Should use terms classes when i am talking about object here");
+            Console.WriteLine("There's several different classes or objects that that could do what we need in a given situation. Perhaps some are better suited than others in a certain situation");
+            Console.WriteLine("There might be some interchangebility in the objects or rather the classes that we ll rely on to fill a specific role in our system");
+            Console.WriteLine("We choose one, the one that's best suited for our specific need in given area of the system.We rely on it to do its job potentially milions of times without fail");
+            Console.WriteLine("Now that object will play an inportant role in our conceptual machine in our system.");
+            Console.WriteLine("An object should really be good at just doing a few things");
+            Console.WriteLine("And the scope of its capabilities should be fairly narrow focused");
+            Console.WriteLine("We might call a well designed very focused object highly cohesive because its entire construction ,its properties, its methods, its events. they are all focused on just accomplishing one little thing");
+            Console.WriteLine("Coonversely if we had a single machine capable of taking on several different responsabilities on our assembly line, it sound great, but when we need to maintain that machine");
+            Console.WriteLine("This is going to be more complex to fix it whenver its breaks.Is it flexible enought to do everything we will ever need in the future.");
+            Console.WriteLine("Is it possible that we are limited ourselves and we are made it more difficult to take that one machine out of our factory and replace it with smaller more focused mashines because we rely on it to do so much fo us ");
+            Console.WriteLine("We may describe that machine as low cohesion because it is doing all bu herself ");
+            Console.WriteLine();
+            Console.WriteLine("So in general in object-oriented programming we want to assign responsabilities to very focused objects that can do one thing and they do them very well");
+            Console.WriteLine("That object will play an important role in the system. But if our needs ever change over time, it;s good to know that we can swap out a given object and give it its responsabilities to a similar but diffrenet object");
+            Console.WriteLine("That can perform the same role and do the same responsabilities but just do it a little bit differenty- but have little impact on the overall operation of the conceptual system");
+            Console.WriteLine("Liskoff substitution principle . The L in the acronym solid  ");
+            Console.WriteLine("Perhaps the most important set of principles for modern day object oriented programming ");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+// https://www.youtube.com/watch?v=v_RcAeRSbI8&list=PLqT8wAi-pOx7X3i6Vynm57MCwFpQWJdP9&index=8 
