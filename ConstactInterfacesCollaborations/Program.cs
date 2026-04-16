236d32c8d365c3fc2828e316ab91dc1b417b4164
Cvijander <cvija85@gmail.com>
2026-04-16 08:13:15 +0200

    interfaces, contract, collaboration

---
+++ ConstactInterfacesCollaborations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConstactInterfacesCollaborations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Collaboration is the idea that objects work together to complete a task");
+            Console.WriteLine("Each object has responsabilities and plays a role in the system and this necessitates that each object gets along well with others");
+            Console.WriteLine("The way they collaborate is typically through the sending of messages back and forth");
+            Console.WriteLine("What makes object oriented programming interesting and useful is mesaging, collaboration, conversations between objects to complete a task ");
+            Console.WriteLine("Machine has no such inteligence to ask questions throught unknowns ");
+            Console.WriteLine("All it knows is that it expects input in one form and creates output potentially in a different form");
+            Console.WriteLine("If it does not get exactly what it needs withing a very narrow set of parameters, it stops");
+            Console.WriteLine();
+            Console.WriteLine("Now in object oriented programming you can think of a contract in one of several ways. ");
+            Console.WriteLine("The first and the easiest way to think about contract , when we are thinking about objects and classes and properties is to thnik about method signature");
+            Console.WriteLine("If you want me to do that task that method, then i am going to need this information fomr you and you can expect back from me this information");
+            Console.WriteLine("I am going to expect these input parameters, and then when it s all said and done, i m going to return back to you a variable or an object of this type.");
+            Console.WriteLine("Now, if i cant' figure it out, i will let you know, I will raise and exception");
+            Console.WriteLine("This is how object can communicate to each other a contract is throught a method signature");
+            Console.WriteLine();
+            Console.WriteLine("There is a notion of interface ");
+            Console.WriteLine("This is a  special progammatic construct that defines a set of properties and methods and eventis and son that  a class must implement in order to take on a given task");
+            Console.WriteLine("If you think about method signature and an interface over time you will begin to realize that they are two sides of the same coins");
+            Console.WriteLine("The name that was attributed to this interface. It starts with the letter I by convention, also by convention it uses the suffix \"able\"");
+            Console.WriteLine("Third ,there is two sides to a contract and then there is the contract itself . On the one side, you have an object that knows how to work with an object that implements I equitable");
+            Console.WriteLine("On the other side you have the object that actually does the implementing. It actually implements IEquable");
+            Console.WriteLine("In order to implement this interface this contract of I equatable it is implementing a method called equals , but there is not method body ");
+            Console.WriteLine("All we are saying is that there has to be an implementation of this equals method, it has to return a boolean. It has to accept the type of T, whatever the T is of given object, ");
+            Console.WriteLine("In order for this to be a class that implements I euatable");
+
+            Console.WriteLine("\"No man is an island \"");
+            Console.WriteLine("When you think of object you shoudl be thinking in those terms as well. Each object has responsabilities in its conceptual society and it has to collaborate with other objects in order to perform work");
+            Console.WriteLine("In order to collaborate there has to be some sort of contract upfront, is the conceptual world this takes the form of method signatures");
+            Console.WriteLine("It also take the form of interfaces");
+            Console.WriteLine("We have defined a contract, we implemented a contract, and then we called a method ");
+            Console.WriteLine("We are not worried about how it is implemented, just the fact thet if we need this functinlaty, then we have to make sure that we are relying on this interface");
+            Console.WriteLine("And then th");
+
+            Console.WriteLine("prodji zadnji mutut !!!");
+
+
+            Console.ReadLine();
+        }
+    }
+}
