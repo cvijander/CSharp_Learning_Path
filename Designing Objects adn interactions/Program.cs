5feffc9c2aee2d04d9fa392ee03766c96c206d38
Cvijander <cvija85@gmail.com>
2026-04-23 08:03:49 +0200

    Lession 16 Encapsulation - start

---
+++ Designing Objects adn interactions/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Designing_Objects_adn_interactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How do we design individual objects in our conceptual machinery? How do we know whne our objects are well defined at all");
+            Console.WriteLine("How do we know that the object is not overreaching its boudaries of its domain or apllication specific responsibilities ?");
+            Console.WriteLine();
+            Console.WriteLine("There is not single answer to this question");
+            Console.WriteLine("A well defined object supports a clearly defined role. And that design is an iterative and inceremental process of envisioning objects and their responsibilities and inventing flexible collaborations");
+            Console.WriteLine("Fortunaly, we do have some patterns and principles that help us discover elegant ways of handling common problems");
+            Console.WriteLine("I mean common interactions or collaborations between objects and they have been left to us by practitioners over the course of the last several decades");
+            Console.WriteLine("We get to decide as developers, we get to apply that advice. WE get to apply those patterns and those principles situationally to our own problem domain");
+            Console.WriteLine("This is not a one-time process,but it is an ongoing process ");
+            Console.WriteLine("We should never take the attitude that our decisions are final");
+            Console.WriteLine("Everything should always be opet for re-evaluation");
+            Console.WriteLine("We have to be willing to and able to refactor our code when a requirent demands that we consider the new practice or pattern");
+            Console.WriteLine();
+            Console.WriteLine("What are these principles and patterns that we should at least consult before we go off and make a decision specific based on our problem domain");
+            Console.WriteLine();
+            Console.WriteLine("The sources for the priciples is the kind of the mother of them all is from the gang of four  ");
+            Console.WriteLine("It a book entitled \"Design Patterns\" - four authors that have been instrumental in helping to document design patterns ");
+            Console.WriteLine("Now the challenge with this book is that it is very difficult to read ");
+            Console.WriteLine("And it is typically written out in eighter Java or C++, some of the terminology they used are over my head");
+            Console.WriteLine();
+            Console.WriteLine("The next book  \"Appying uml and patterns\" by Craig Lman  ");
+            Console.WriteLine("3 rd book to read is  from Uncle Bob Martin - \" Agile, Principles,Patterns and Practices in C#\" ");
+            Console.WriteLine();
+            Console.WriteLine("4 th book that Bob recommend is \"Head First Design patterns\" by O 'Relly");
+            Console.WriteLine("Book \"Design pattern\" is more technical and difficult to read, but the rest of them ,especially Uncle Bob is for C# written is more suitable for .NET");
+            Console.WriteLine();
+            Console.WriteLine("You can really spend a lifetime mastering all of this. And  any road that you decide to kind of go off on a tangent , it will pretty much lead out info infinity");
+            Console.WriteLine();
+            Console.WriteLine("But it all comes back to some basic tenants . And if you can get those right, ytou can probably build your application in such a way tyhat it can withstand the inevitable impact of change on the software system");
+            Console.WriteLine("If we can just get that under our belt, then we will be doing fine . You can make a career out of all of this. ");
+            Console.WriteLine("Many people have , it is unfortunately both appeling and dauting at the same time because there is just to much to learn and so little time to learn it");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+// https://www.youtube.com/watch?v=Mzhdfj9VBLM&list=PLqT8wAi-pOx7X3i6Vynm57MCwFpQWJdP9&index=15
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Object oriented programming is all about boundaries and relationships and communication across those bondaries");
+            Console.WriteLine("Through the relationships that objects have with one another");
+            Console.WriteLine();
+            Console.WriteLine("The major tenets of object-oriented programming");
+            Console.WriteLine("The word tenet is not a word that we use frequently. It is defined as a principle or a belief, especially one of the main pricniples of a given religion or philosophy");
+            Console.WriteLine("We use the term tenant to express some of the highest guiding principles and concepts in object-oriented programming from which all the other things flow that we will learn about");
+            Console.WriteLine();
+            Console.WriteLine("There are four tenets of object-oriented programming.");
+            Console.WriteLine("1. \"Encapsulation\"");
+            Console.WriteLine("2. \"Inheritance \" ");
+            Console.WriteLine("3. \"Polymorphism \" ");
+            Console.WriteLine("4. \"Abstraction \"");
+            Console.WriteLine();
+            Console.WriteLine("Encapsulation ");
+            Console.WriteLine("Encapsulation is a mechanism that is used to hide the implemetation details of a class");
+            Console.WriteLine("This will take several different forms");
+            Console.WriteLine("Class is made up of information, typically fields of information , and then it is made of functions which we call  object oriented methos ");
+            Console.WriteLine("Those methods operate on the information the fields, or use that infomration in some way to perform a task that we have delegated to it");
+            Console.WriteLine("It has a responsability");
+
+            Console.WriteLine("oko 244 - ali proci ponovo ");
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+// https://www.youtube.com/watch?v=FG-JDZ5FJa0&list=PLqT8wAi-pOx7X3i6Vynm57MCwFpQWJdP9&index=16
