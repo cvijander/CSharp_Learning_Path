fc65b7e34e235fb865a47614c5268a23d40b31aa
Cvijander <cvija85@gmail.com>
2026-04-28 06:50:07 +0200

    Polymorphysm

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -23,12 +23,31 @@
             Console.WriteLine("For automobile, we will get the same display we had previous, but for suv we will get the display from car which does not contain the drive method");
             Console.WriteLine("We are getting cars version for display method");
             Console.WriteLine();
-            Console.WriteLine("But if we in display method in car put virtual  then in display method in suv instead new we put override ");
+            Console.WriteLine("In order to enable polymorphism in our code, we neeed to make two modifications to our code");
+            Console.WriteLine("But if we in display method in car put \"virtual\"  then in display method in suv instead \"new\" we put \"override\" ");
+            Console.WriteLine("This method can be completely overridden, notjust hidden but it can be even exteneded by derived class");
+            Console.WriteLine("We are adding \"virtual\" keyword ");
+            Console.WriteLine("Now i derived class we are replacing word \" new \" with word \"override\"");
+            Console.WriteLine();
             Console.WriteLine("Then we will get the same result on the starting point ");
+            Console.WriteLine("We see the polymorphic behaviour and it behaves correctly");
+            Console.WriteLine();
+            Console.WriteLine("It is the ability for you to use or to call a given method across multiple different types and for it to actually make a call in and use that particual version of that method");
+            Console.WriteLine();
+            Console.WriteLine("Polymorphism is our manifestation of our desire to build more flexible collaborations between objects");
+            Console.WriteLine("Inflexible collaborations are limiting");
+            Console.WriteLine("We are really not relying on a concrete direct reference to a specific object");
+            Console.WriteLine("Here in our print method, we are just saying anything that comes down line from automobile. It can be automobile, suv, or engine");
+            Console.WriteLine("We are not relying on specific object per se in order to call that method on than objec");
+            Console.WriteLine("As long as the object that we pass in her has a method defined with that specific name then it has that specific input and it returns that particular return type");
+            Console.WriteLine();
+            Console.WriteLine("With polymorphysm - We decoupled the collaboration");
 
 
 
-            Console.WriteLine("208 - 715");
+
+
+            
 
             var car = new Automobile { Make = "Hyundai", Model = "Elantra", Year = 2010 };
            // car.Display();
