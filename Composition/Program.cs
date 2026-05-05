a617e6f1addb59545f1fce15bf62ae2d724d785e
Cvijander <cvija85@gmail.com>
2026-05-05 08:09:10 +0200

    Lession 23 Composition

---
+++ Composition/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace Composition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Final relatioship related topics for this course");
+            Console.WriteLine();
+            Console.WriteLine("1.  Association , a normal healthy relationship between two objects");
+            Console.WriteLine("2.  Aggregation, which is a more permanent relationship where one object keeps references to other instances of objects");
+            Console.WriteLine("However if that object were to go away, the objects that it references dont necessarily go away");
+            Console.WriteLine("There are two properties of the relationship. There is life cycle , do these live on their own and die on their own");
+            Console.WriteLine("Second there is ownership where at some point in time one object will essentialy own references to other ones");
+            Console.WriteLine();
+            Console.WriteLine("3.  Composition , which is a stronger version of aggregation");
+            Console.WriteLine("In composition, the child simply cannot exist without its parent");
+            Console.WriteLine("House and rooms . If you destroy the house, the rooms will be also destroyed");
+            Console.WriteLine("House and room class, room class is designed inside the boundaries of a house class");
+            Console.WriteLine("We have defined a class inside a class, that is one way of denoting ownership of one object to another");
+            Console.WriteLine("Composition is both ownership and it denotes lifetime");
+            Console.WriteLine("When we destroy the house, the kitchen should be gone as well");
+            Console.WriteLine("We have to change the access modifier from public to protected in room class");
+            Console.WriteLine("Composition is an interesting way to delegate responsabilities to your application");
+            Console.WriteLine();
+            Console.WriteLine("Well known idiom  which says favor \"composition over ihneritance\"");
+            Console.WriteLine("It is an advice because word favor is used , which means you shoudl prefer");
+            Console.WriteLine("Wikipedia page - Composition over inheritance");
+            Console.WriteLine("We are calling implementation inheritance , not interface inheritance");
+            Console.WriteLine("It says to favor composition over inheritance, is a design principle that gives the design higher flexibility");
+            Console.WriteLine("It is more natural to build bussiness domain classes out of various components which means classes than trying to find commonality between them nad creating a family tree");
+            Console.WriteLine("Gas pedal and a wheel are both vital components of a car. ");
+            Console.WriteLine("Composition also provides a more stable bussiness domain in the long term as it is less prone to the quirks of the family members");
+            Console.WriteLine("It is better to compose what an object can do with  \" a has a \" relationship than extend what it is with an \"is a \" relationship that you get with inheritance");
+            Console.WriteLine("Interface inheritance that the initial design is simplified by identifing system object baheviours in separate interfaces instead of creating a hierarhical relationship to distribute behaviours among bussiness domain classes via inheritance");
+            Console.WriteLine("This approach more easily accommodates future requiremnt changes that would otherwise require complete restructuring of the bussiness domain classes in the inheritance model");
+            Console.WriteLine("Additionaly it avoids problems often associated with relatively minor changes to an inheritance based model that includes several generations of classes");
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("1239 - 1456");
+
+            var house = new House();
+
+            //  var room = new House.Room() { Name = "Kitchen" };
+            // house.Rooms.Add(room);
+
+            house.AddRoom("Kitchen");
+
+            house.Display();
+
+            Console.WriteLine(house.RoomsCount());
+
+            // Important 
+
+            house = null;
+
+            Console.WriteLine("House is destroyed here");
+
+           //Console.WriteLine(house.RoomsCount());
+          //  room.Display();
+
+            Console.ReadLine();
+        }
+    }
+
+    class House
+    {
+        protected List<Room> Rooms { get; set; }
+
+        public House()
+        {
+            Rooms = new List<Room>();
+            var room1 = new Room() { Name = "Bedroom 1 " };
+            var room2 = new Room() { Name = "Bedroom 2 " };
+            var room3 = new Room() { Name = "Bedroom 3 " };
+            Rooms.Add(room1);
+            Rooms.Add(room2);
+            Rooms.Add(room3);
+        }
+
+        public void AddRoom(string name)
+        {
+            var room = new Room() { Name = name };
+            Rooms.Add(room);
+        }
+
+        public int RoomsCount()
+        {
+           return  Rooms.Count;
+        }
+
+        public void Display()
+        {
+            foreach (var room in Rooms)
+            {
+                room.Display();
+            }
+        }
+
+        // to get this to work as a true Composition
+        // example, you have to change from Public
+        // to Protected
+        // A protected member is accessible within its class 
+        // and by derived class intances.
+        // In this case, it won't be accessible to the Program class 
+
+        protected class Room
+        {
+            public string Name { get; set; }
+
+            public void Display()
+            {
+                Console.WriteLine($"Room: {Name}");
+            }
+        }
+    }
+}
+
+
+// https://www.youtube.com/watch?v=4k50veECCHw&list=PLqT8wAi-pOx7X3i6Vynm57MCwFpQWJdP9&index=23
--- aggregation/Program.cs
+++ aggregation/Program.cs
@@ -6,13 +6,13 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Objects sometimes merely collaborate");
-            Console.WriteLine("One object need help, it requests help from another object. After they collaborate, the task at hand, thet move on with their lives");
+            Console.WriteLine("One object need help, it requests help from another object. After they collaborate, the task at hand, they move on with their lives");
             Console.WriteLine("Sometimes , objects share a tighter bond.Like we had in previous lessions ");
             Console.WriteLine();
             Console.WriteLine("There are two special types of relationships that we are going to look at next ");
             Console.WriteLine("Association - that is where you have just a normal collaboration");
-            Console.WriteLine("There is relationship between two objects, but esch of them ultimately have their own life cycle");
-            Console.WriteLine("They dont own each other . There is no other futher relationship between just one time association");
+            Console.WriteLine("There is relationship between two objects, but each of them ultimately have their own life cycle");
+            Console.WriteLine("They dont own each other . There is no other futher relationship between, just one time association");
             Console.WriteLine();
             Console.WriteLine("Aggregation - this is sometimes known as a \"HAS A \" style relationship between objects ");
             Console.WriteLine("It denotes ownership or whole part or a this object is a part of this larger object");
@@ -22,15 +22,92 @@
             Console.WriteLine("If a teacher were to be eliminated it would not eliminatie the department that she belonged to");
             Console.WriteLine("And vice versa, if you were to eliminate the department, the teacher could find another job at another department in the university");
             Console.WriteLine("But the teacher would still exist");
-            Console.WriteLine("We are really talkinh about two properties of the relationship");
+            Console.WriteLine();
+            Console.WriteLine("We are really talking about two properties of the relationship");
             Console.WriteLine("One is life cycle, when do they live and when do they die");
             Console.WriteLine("The other one is ownerships");
+            Console.WriteLine("In aggregation there is and ownership between one object and another object, but there is not a direct correlation of life cycle");
+            Console.WriteLine("They live and die independent of eachother");
+
+            var car1 = new Automobile();
+            var car2 = new Automobile();
+            var car3 = new Automobile();
+
+            var person = new Person() { Name = "Bob" };
+
+
+            var fleet = new Fleet();
+
+            fleet.Automobiles.Add(car1);
+            fleet.Automobiles.Add(car2);
+            fleet.Automobiles.Add(car3);
+
+            fleet.Owner = person;
+
+            foreach (var auto in fleet.Automobiles)
+            {
+                auto.Display();
+            }
+
+            fleet.Owner.Display();
+
+            // IMPORTANT
+            fleet = null;
+
+            Console.WriteLine("Do the aggregated objects still live ? ");
+
+
+            car1.Display();
+            car2.Display();
+            car3.Display();
+
+            person.Display();
 
 
             Console.ReadLine();
 
         }
     }
+
+    class Automobile
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"Auto: {Make}  {Model} {Year}");
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"Person: {Name}");
+        }
+
+    }
+    class Fleet
+    {
+        public string Name { get; set; }
+
+        public List<Automobile> Automobiles { get; set; }
+
+        public Person Owner { get; set; }
+
+        public Fleet()
+            {
+            Automobiles = new List<Automobile>();
+        }
+    }
+
+
 }
 
 
