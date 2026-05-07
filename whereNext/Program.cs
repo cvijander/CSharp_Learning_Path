ea0f0325de72490d76bb55753e964e268c37d917
Cvijander <cvija85@gmail.com>
2026-05-07 08:11:46 +0200

    Lession 25 Where to go from here

---
+++ whereNext/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace whereNext
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The goal for this course was to help you understand \"the why\" before we started really talking in detail \"How do I \" ");
+            Console.WriteLine("Object oriented programming is all about agonizing over the details of delegating responsabilities and designing collaborations between objects in order to create a conceptual machine");
+            Console.WriteLine("One skill is to understand the objects , you must become the object sort of thing , to understand his responsabilitis , etc");
+            Console.WriteLine("Programming like an object oriented programmer ");
+            Console.WriteLine("UML, class diagrams, sequence diagrams ");
+            Console.WriteLine("Solid principles and patterns");
+            Console.WriteLine("Domain driven design");
+            Console.WriteLine("Gang of 4 - patterns");
+            Console.WriteLine("Agile methodology");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+// https://www.youtube.com/watch?v=CRB7pslUlV8&list=PLqT8wAi-pOx7X3i6Vynm57MCwFpQWJdP9&index=25
