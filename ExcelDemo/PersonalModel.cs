8d4dd7341217ef13ec9327b023458de3c9044104
Cvijander <cvija85@gmail.com>
2026-05-08 08:05:34 +0200

    Saving data to an excel file

---
+++ ExcelDemo/PersonalModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ExcelDemo
+{
+    public class PersonalModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ ExcelDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using OfficeOpenXml;
+
+namespace ExcelDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ExcelPackage.License.SetNonCommercialPersonal("Borko");
+            var file = new FileInfo(@"D:\Dev\csharp\CSharp_Learning_Path\IAmTimCorey\excelFile.xlsx");
+
+            var people = GetSetupData();
+
+            await SaveExcelFile(people, file);
+        }
+
+        private static async Task SaveExcelFile(List<PersonalModel> people, FileInfo file)
+        {
+            DeleteIfExists(file);
+
+            using var package = new ExcelPackage(file);
+            
+            var ws = package.Workbook.Worksheets.Add("MainReport");
+
+            var range = ws.Cells["A1"].LoadFromCollection(people, true);
+
+            range.AutoFitColumns();
+
+            await package.SaveAsync();
+        }
+
+        private static void DeleteIfExists(FileInfo file)
+        {
+           if (file.Exists)
+            {
+                file.Delete();
+            }
+        }
+
+        private static List <PersonalModel> GetSetupData()
+        {
+            List<PersonalModel> output = new()
+            {
+                new PersonalModel() { Id = 1, FirstName ="Tim", LastName = "Corey"},
+                new PersonalModel() { Id = 2, FirstName = "Sue", LastName = "Storm"},
+                new PersonalModel() { Id = 3 , FirstName = "Jane", LastName = "Smith"}
+
+
+            };
+
+            return output;
+        }
+    }
+}
